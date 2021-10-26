f14844015f9bff38d0aa208020c0c3ede7b475be
Vladyslav Aviedov <vladaviedov@protonmail.com>
2021-10-25 20:53:28 -0400

    removed unused imports

--- src/main/java/frc/robot/commands/ManualDrive.java
+++ src/main/java/frc/robot/commands/ManualDrive.java
@@ -8,8 +8,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-/**import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;*/
-import frc.robot.Robot;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.Constants;
--- src/main/java/frc/robot/commands/MoveToFire.java
+++ src/main/java/frc/robot/commands/MoveToFire.java
@@ -7,7 +7,6 @@
 
 package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.Robot;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.Gyroscope;
 
