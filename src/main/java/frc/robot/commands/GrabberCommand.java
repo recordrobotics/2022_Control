50761a55f3fc04edcb919d24222392e4f609d202
Vladyslav Aviedov <vladaviedov@protonmail.com>
2021-12-13 20:41:48 -0500

    implemented some grabber cmd

--- src/main/java/frc/robot/commands/GrabberCommand.java
+++ src/main/java/frc/robot/commands/GrabberCommand.java
@@ -1,12 +1,14 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-
+import frc.robot.OI;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.AcquisitionMonolith;
 
 public class GrabberCommand extends CommandBase{
+
     private AcquisitionMonolith m_aquisition = (AcquisitionMonolith) RobotContainer.getInstance().getAcquisition();
+    private OI m_oi = RobotContainer.getInstance().getOi();
 
     public GrabberCommand() {
       addRequirements(m_aquisition);
@@ -15,12 +17,16 @@ public class GrabberCommand extends CommandBase{
     /** Called just before this Command runs the first time */
     @Override
     public void initialize() {
-      
+      m_aquisition.stopMotors();
+      m_aquisition.setGrabSolenoidOff();
     }
   
     /** Called repeatedly when this Command is scheduled to run */
     @Override
     public void execute() {
+      double maxGrabberSpeed = 1d;
+
+      // TODO: implement
     }
   
     /** Make this return true when this Command no longer needs to run execute() */
@@ -32,5 +38,7 @@ public class GrabberCommand extends CommandBase{
     /** Called once after isFinished returns true */
     @Override
     public void end(boolean interrupted) {
+      m_aquisition.stopMotors();
+      m_aquisition.setGrabSolenoidOff();
     }
 }
