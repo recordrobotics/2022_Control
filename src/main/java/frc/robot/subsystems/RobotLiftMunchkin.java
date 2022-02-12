800ce886e1cca324a085170cfba41e1b0117d0c9
Rit <58277581+rgarg23@users.noreply.github.com>
2022-02-12 16:48:49 -0500

    Made new file for Munchkin's lift (currently exact replica of 2020)

---
+++ src/main/java/frc/robot/subsystems/RobotLiftMunchkin.java
@@ -0,0 +1,50 @@
+/*Info on Robot Lift:
+1 CIM - + and - (circular) direction, possible neutral (spins freely)
+1 MINI CIM - + and - (circular) direction
+Both motor control different systems
+Possible thought of combining both lifts
+package frc.robot.subsystems;*/
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogEncoder;
+import com.ctre.phoenix.motorcontrol.*;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import frc.robot.RobotMap;
+import frc.robot.Constants;
+
+public class RobotLiftMunchkin extends RobotLift{
+    private WPI_VictorSPX robotLiftMotor = new WPI_VictorSPX(RobotMap.robotLiftLeftMotorPort);
+    private double targetVoltage = Constants.robotLiftTargetVoltage;
+
+    public RobotLiftMunchkin(){
+        robotLiftMotor.enableVoltageCompensation(true);
+        robotLiftMotor.setVoltage(targetVoltage);
+    }
+    /**
+     * robotLiftMotorLeft creates variable for the left lift motor.
+     * stop() stops the motor.
+     */
+  
+    AnalogInput encoderInput = new AnalogInput(0);
+    AnalogEncoder liftEncoder = new AnalogEncoder(encoderInput);
+
+    public void stop() {
+
+        robotLiftMotor.stopMotor();
+
+        robotLiftMotor.set(0.0);
+    }
+    public double getPosition(){
+        return liftEncoder.get();
+    }
+    /**
+     * moveLift() moves lift up and down.
+     * @param v how fast the left lift motor spins.
+     */
+    public void moveLift(double v) {
+
+        robotLiftMotor.set(ControlMode.PercentOutput, v);
+    }
+}
