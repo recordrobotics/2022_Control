7fe4c03144d1f8485727078ceda9f726e0acff2d
DragonSM <57463312+DragonSM@users.noreply.github.com>
2022-02-19 13:45:12 -0500

    Re-adding bits of acquisition stuff

---
+++ src/main/java/frc/robot/subsystems/AcquisitionMunchkin.java
@@ -0,0 +1,104 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import frc.robot.RobotMap;
+import frc.robot.commands.ControlAcquisition; // in commented-out code
+import frc.robot.Constants;
+
+import com.ctre.phoenix.motorcontrol.*;
+import com.ctre.phoenix.motorcontrol.can.*;
+import com.revrobotics.CANSparkMax;
+
+public class AcquisitionMunchkin extends Acquisition {
+    /**
+     * aquireMotor tiltMotor Creating variables for the acquisition's motors.
+     * tiltLimit The maximum angle the acquisition can be at (to avoid unwanted accidents with the acquisition hitting something).
+     * tiltSpeed How fast the aquisition tilts.
+     * tiltPostition Whether the aquisition is up or down.
+     */
+
+    private CANSparkMax acquireMotor = new CANSparkMax(RobotMap.acquireMotorPort, null);
+    private CANSparkMax tiltMotor = new CANSparkMax(RobotMap.acquireMotorPort, null);
+    private double aquireMotorVoltage = Constants.Acq2020AcquireMotorVoltage;
+    private double tiltMotorVoltage = Constants.Acq2020TiltMotorVoltage;
+    DigitalInput tiltLimit;
+
+
+    private double tiltSpeed = 0.5;
+    boolean tiltPosition = true;  /**true is up, false is down*/
+    /**
+     * Creates an acquisition object with a specific tilt limit.
+     */
+    public AcquisitionMunchkin(){
+        
+    }
+    /**
+     * Gets how fast the acquisition is tilting.
+     * @return returns said speed.
+     */
+    public double getTiltSpeed(){
+        return tiltSpeed;
+    }
+    /**
+     * Gets where the acquisition is in its tilt path.
+     * @return returns said position.
+     */
+    public boolean getTiltPosition() {
+        return tiltPosition;
+    }
+    /**
+     * Sets the acquisition's tilt.
+     * @param pos whether the tilt should be up or down.
+     */
+    public void setTiltPosition(boolean pos){
+        tiltPosition = pos;
+    }
+    /**
+     * Spins the acquisition motor.
+     * @param v speed to spin the acquisition at.
+     */
+    public void moveAcq(double v) {
+        acquireMotor.set(v);
+    }
+    /**
+     * Moves the acquisition up and down.
+     * @param v speed of the motor.
+     */
+    public void moveTilt(double v) {
+        tiltMotor.set(v);
+    }
+    /**
+     * Returns how far the acquisition can tilt.
+     * @return how far the acquisition can tilt.
+     */
+    public boolean getTiltLimit(){
+        return tiltLimit.get();
+     }
+     /*
+    public double getAngle(){
+        return acqEncoder.get() * 360/174.9;
+    }
+    */
+    /**
+     * Returns if the acqusition is spinning.
+     * @return is the motor running.
+     */
+    public boolean isAcqOn(){
+        return acquireMotor.get() > 0;
+    }
+    
+    
+    public double getAcquireVoltage(){
+        return acquireMotor.getVoltageCompensationNominalVoltage();
+    }
+    public double getTiltVoltage(){
+        return tiltMotor.getVoltageCompensationNominalVoltage();
+    }
+}
