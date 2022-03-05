09b4ef019a7910bd14e0453c239a18e8723e2bb7
reset <reset>
2022-03-04 20:59:20 -0500

    Partial stuff for servos that we might have to delete anyways

---
+++ src/main/java/frc/robot/commands/ControlMunchkinServos.java
@@ -0,0 +1,46 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.AcqServosMunchkin;
+
+/**
+ * An example command. You can replace me with your own command.
+ */
+public class ControlMunchkinServos extends CommandBase {
+
+  private AcqServosMunchkin m_servos = RobotContainer.getInstance().getAcqServos();
+  public ControlMunchkinServos() {
+    /** Use robot container to declare the subsytem's default command */
+  }
+
+  /** Called just before this Command runs the first time */
+  @Override
+  public void initialize() {
+  }
+
+  /** Called repeatedly when this Command is scheduled to run */
+  @Override
+  public void execute() {
+    m_servos.setServos(0);
+  }
+
+  /** Make this return true when this Command no longer needs to run execute() */
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  /** Called once after isFinished returns true */
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+}
