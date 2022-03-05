3b56ca3428a9343eb5683e33eef544f0f3889064
fielia <rit.g.garg@gmail.com>
2022-03-05 15:18:11 -0500

    Fixed small error in LiftControl.java

--- src/main/java/frc/robot/commands/ControlFlywheel.java
+++ src/main/java/frc/robot/commands/ControlFlywheel.java
@@ -76,12 +76,4 @@ public class ControlFlywheel extends CommandBase {
     return false;
   }
 
-  /** Called once after isFinished returns true*/
-  
-
-/**
-*   Called when another command which requires one or more of the same
-*   subsystems is scheduled to run
-*/
-  
 }
