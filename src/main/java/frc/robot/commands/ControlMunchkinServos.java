29425a04e5f13e28801de8e9a06c853fb0394911
cabbageOfRecordRobotics <programmingcabbage@gmail.com>
2022-03-07 19:40:52 -0500

    files for servos in the ball channel that may or may not exist should (in theory) now work

--- src/main/java/frc/robot/commands/ControlMunchkinServos.java
+++ src/main/java/frc/robot/commands/ControlMunchkinServos.java
@@ -8,6 +8,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.OI;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.AcqServosMunchkin;
 
@@ -15,7 +16,7 @@ import frc.robot.subsystems.AcqServosMunchkin;
  * An example command. You can replace me with your own command.
  */
 public class ControlMunchkinServos extends CommandBase {
-
+  private boolean prevToggle = false, servosFlipped = false;
   private AcqServosMunchkin m_servos = RobotContainer.getInstance().getAcqServos();
   public ControlMunchkinServos() {
     /** Use robot container to declare the subsytem's default command */
@@ -24,12 +25,24 @@ public class ControlMunchkinServos extends CommandBase {
   /** Called just before this Command runs the first time */
   @Override
   public void initialize() {
+    m_servos.setServos(0);
   }
 
   /** Called repeatedly when this Command is scheduled to run */
   @Override
   public void execute() {
+    if ((OI.getXboxButtonState("X") != prevToggle) && (OI.getXboxButtonState("X"))){
+      servosFlipped = !servosFlipped;
+      System.out.println("toggle! " + servosFlipped);
+    }
+
+    if (servosFlipped){  
+    m_servos.setServos(1);
+    } else {
     m_servos.setServos(0);
+    }
+
+    prevToggle = OI.getXboxButtonState("X") ;
   }
 
   /** Make this return true when this Command no longer needs to run execute() */
