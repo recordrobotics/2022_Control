95986ece888aca7f4fde7afb778538d8dce50cd3
cabbageOfRecordRobotics <programmingcabbage@gmail.com>
2022-03-18 20:55:22 -0400

    added curve for rotatlift

--- src/main/java/frc/robot/commands/ControllerRotateLift.java
+++ src/main/java/frc/robot/commands/ControllerRotateLift.java
@@ -32,15 +32,23 @@ public class ControllerRotateLift extends CommandBase {
   /** Called repeatedly when this Command is scheduled to run */
   @Override
   public void execute() {
-    if(OI.getRightStickLeft()){
-      moveRotater(ROTATOR_SPEED);
-      System.out.print("Moving Left Pressed");
-    } else if(OI.getRightStickRight()){
-      moveRotater(-ROTATOR_SPEED);
-      System.out.print("Moving Right Pressed");
-    } else {
-        moveRotater(0);
+    double speed = OI.getCStickXAxis();
+    if (Math.abs(speed) < 0.15) {
+      speed = 0;
     }
+    else if (Math.abs(speed)> 0.5) {
+      speed = Math.abs(speed)/(speed*2);
+    }
+    moveRotater(speed);
+    // if(OI.getRightStickLeft()){
+    //   moveRotater(ROTATOR_SPEED);
+    //   System.out.print("Moving Left Pressed");
+    // } else if(OI.getRightStickRight()){
+    //   moveRotater(-ROTATOR_SPEED);
+    //   System.out.print("Moving Right Pressed");
+    // } else {
+    //     moveRotater(0);
+    // }
     
   }
 
